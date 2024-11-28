58519e921f7da5964d7d3c5ab6dd0e10f8264ae2
AathiEswar <aathi28082003@gmail.com>
2024-11-28 13:00:43 +0530

    primary and qualifier

--- starter/src/main/java/com/spring/starter/SpringStarter01.java
+++ starter/src/main/java/com/spring/starter/SpringStarter01.java
@@ -5,7 +5,13 @@ import com.spring.starter.starter01.Person;
 import com.spring.starter.starter01.Starter01Configuration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-// Spring IoC container - Creates , configures , assembles, and manages life cycle of all the components
+import java.util.Arrays;
+
+// POJO - Plain old Java Object
+// Java Bean - no arg constructor , getters and setters , implements serializable
+// Spring Bean - any object managed by Spring
+
+// Spring IoC container(Bean/Application) - Creates , configures , assembles, and manages life cycle of all the components
 /*
 *  ------------------------------------------------------------
 *  | JVM                                                       |
@@ -22,7 +28,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class SpringStarter01 {
     public static void main(String[] args){
         // 1 : Start a Spring Context
-
         // Ioc Container for Spring
         // helps for scanning and registering the beans (accepts @Configuration and @Component )
         var context = new AnnotationConfigApplicationContext(Starter01Configuration.class);
@@ -30,14 +35,17 @@ public class SpringStarter01 {
 
         // ACCESS a bean
         // using its name
-        System.out.println(context.getBean("name"));
-        System.out.println(context.getBean("age"));
-        System.out.println(context.getBean("personRecord"));
-        System.out.println(context.getBean("personMethodCall"));
+//        System.out.println(context.getBean("name"));
+//        System.out.println(context.getBean("age"));
+//        System.out.println(context.getBean("personRecord"));
+//        System.out.println(context.getBean("personMethodCall"));
         System.out.println(context.getBean("personParameters"));
-        System.out.println(context.getBean("address1"));
+//        System.out.println(context.getBean("address1"));
 
         // using its type
-        System.out.println(context.getBean(Address.class));
+//        System.out.println(context.getBean(Address.class));
+//        System.out.println(context.getBean(Person.class));
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
--- starter/src/main/java/com/spring/starter/starter01/Starter01Configuration.java
+++ starter/src/main/java/com/spring/starter/starter01/Starter01Configuration.java
@@ -1,7 +1,9 @@
 package com.spring.starter.starter01;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 // Mentions this class will be responsible for the configurations
 @Configuration
@@ -16,19 +18,32 @@ public class Starter01Configuration {
         return 21;
     }
     @Bean(name = "personRecord")
+    @Primary
     public Person person(){
         return new Person("Xioa" , 2000 , new Address("Kolkata" , "India"));
     }
     @Bean
-    public Person personParameters(String name, int age, Address address1){
-        return new Person(name , age , address1);
+    public Person personParameters(String name, int age,@Qualifier("address2") Address address){
+        return new Person(name , age , address);
     }
     @Bean
     public Person personMethodCall(){
         return new Person(name() , age() , address());
     }
+
     @Bean(name = "address1")
+    // primary let it be the top choice for auto wiring( dependency injection)
+    @Primary
     public Address address(){
         return new Address("Tamil Nadu" , "India");
     }
+
+    @Bean
+    // Qualifier - Helps for Manual Auto wiring( dependency injection)
+    @Qualifier("address2")
+    public Address address2(){
+        return new Address("Mumbai" , "India");
+    }
+
+
 }
