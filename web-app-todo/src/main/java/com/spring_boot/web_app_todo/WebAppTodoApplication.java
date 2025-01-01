c439e884ad41df7cc1a5332f2471b48fcd967e54
AathiEswar <aathi28082003@gmail.com>
2025-01-01 09:23:54 +0530

    initial web app

---
+++ web-app-todo/src/main/java/com/spring_boot/web_app_todo/WebAppTodoApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.web_app_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppTodoApplication.class, args);
+	}
+
+}
---
+++ web-app-todo/src/test/java/com/spring_boot/web_app_todo/WebAppTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.web_app_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
