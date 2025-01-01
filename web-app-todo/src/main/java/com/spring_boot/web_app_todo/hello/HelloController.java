76dd83a21c6abf8023e87b29871c5fa2508e05ea
AathiEswar <aathi28082003@gmail.com>
2025-01-01 11:15:05 +0530

    hello from jsp

---
+++ web-app-todo/src/main/java/com/spring_boot/web_app_todo/hello/HelloController.java
@@ -0,0 +1,22 @@
+package com.spring_boot.web_app_todo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    // To create a get request with the specified end-point
+    @GetMapping("/say-hello")
+    // To return the exact value and not through any View
+    @ResponseBody
+    public String sayHello(){
+        return "Hello from Controller";
+    }
+
+    @GetMapping("/say-hello-jsp")
+    public String sayHelloJSP(){
+        return "hello";
+    }
+}
---
+++ web-app-todo/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title> THis is from JSP</title>
+    </head>
+    <body>
+        Body Frm JSP
+    </body>
+</html>
