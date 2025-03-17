6197eff01dd6243943e079dccfbfa772936736f7
Aathi Eswar <asiva@fleetstudio.com>
2025-03-17 17:57:55 +0530

    initial webservices

--- web-api/src/main/java/com/spring_boot/web_api/CurrencyServiceConfig.java
+++ web-api/src/main/java/com/spring_boot/web_api/CurrencyServiceConfig.java
@@ -1,2 +1,36 @@
-package com.spring_boot.web_api;public class CurrencyServiceConfig {
+package com.spring_boot.web_api;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "currency-service")
+@Component
+public class CurrencyServiceConfig {
+    private String url;
+    private String key;
+    private String userName;
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
--- web-api/src/main/java/com/spring_boot/web_api/CurrencyServiceController.java
+++ web-api/src/main/java/com/spring_boot/web_api/CurrencyServiceController.java
@@ -1,2 +1,17 @@
-package com.spring_boot.web_api;public class CurrencyServiceController {
+package com.spring_boot.web_api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyServiceController {
+
+    @Autowired
+    CurrencyServiceConfig configCurrency;
+    @RequestMapping(value = "/currency")
+    public CurrencyServiceConfig get(){
+        return configCurrency;
+    }
 }
